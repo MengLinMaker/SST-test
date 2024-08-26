f97e7b20add60394529c883d11d19bc799a0126c
MengLinMaker <menglinmaker@gmail.com>
2024-08-26 17:29:48 +1000

    Init

---
+++ sst.config.ts
@@ -0,0 +1,12 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "sst-test",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {},
+});
