1c7909c52b7d2018296fb1da826c73b48617bd62
MengLinMaker <menglinmaker@gmail.com>
2024-08-26 23:03:45 +1000

    Chore: Config routes

--- sst.config.ts
+++ sst.config.ts
@@ -3,10 +3,21 @@
 export default $config({
   app(input) {
     return {
-      name: "sst-test",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
-    };
+      name: 'sst-test',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws',
+    }
   },
-  async run() {},
-});
+  async run() {
+    const api = new sst.aws.ApiGatewayV2('MyApi')
+    api.route('GET /', {
+      handler: 'functions/index.handler',
+    })
+    api.route('GET /blog', {
+      handler: 'functions/blog.handler',
+    })
+    api.route('GET /account', {
+      handler: 'functions/account.handler',
+    })
+  },
+})
