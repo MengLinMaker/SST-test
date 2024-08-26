2afbfa7f90b5d8307250c0920b814b8f81c66178
MengLinMaker <menglinmaker@gmail.com>
2024-08-26 23:05:07 +1000

    Autogenerate type file

---
+++ sst-env.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+import 'sst'
+declare module 'sst' {
+  export interface Resource {
+    MyApi: {
+      type: 'sst.aws.ApiGatewayV2'
+      url: string
+    }
+  }
+}
